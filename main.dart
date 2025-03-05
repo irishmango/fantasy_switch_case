c8e79c7ba87b5e6cb9ecd36e61083768812bcbd6
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-05 12:51:57 +0100

    outline

---
+++ main.dart
@@ -0,0 +1,22 @@
+enum CharacterClass {warrior, mage, archer}
+enum Weather {sunny, rainy, stormy}
+enum TimeOfDay {morning, noon, evening, night}
+
+void main() {
+  CharacterClass characterClass = CharacterClass.mage;
+  Weather weather = Weather.rainy;
+  TimeOfDay timeOfDay = TimeOfDay.night;
+  int? strength = 38;
+  
+  
+  switch (characterClass) {
+    case CharacterClass.warrior:
+      switch () {
+        case
+      }
+        
+    case CharacterClass.mage:
+      switch()
+      
+    case CharacterClass.archer:
+      switch()
