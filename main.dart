1b24911029e958a89081746489284f8481e4080d
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-05 13:09:42 +0100

    implemented switch logic

--- main.dart
+++ main.dart
@@ -1,22 +1,59 @@
-enum CharacterClass {warrior, mage, archer}
-enum Weather {sunny, rainy, stormy}
-enum TimeOfDay {morning, noon, evening, night}
+enum CharacterClass {Warrior, Mage, Archer}
+enum Weather {Sunny, Rainy, Stormy}
+enum TimeOfDay {Morning, Noon, Evening, Night}
 
 void main() {
-  CharacterClass characterClass = CharacterClass.mage;
-  Weather weather = Weather.rainy;
-  TimeOfDay timeOfDay = TimeOfDay.night;
-  int? strength = 38;
+  CharacterClass characterClass = CharacterClass.Warrior;
+  Weather weather = Weather.Rainy;
+  TimeOfDay timeOfDay = TimeOfDay.Noon;
+  int strength = 38;
   
   
   switch (characterClass) {
-    case CharacterClass.warrior:
-      switch () {
-        case
+    case CharacterClass.Warrior:
+      switch (timeOfDay) {
+        case TimeOfDay.Morning:
+        case TimeOfDay.Noon:
+          strength += 10;
+        case TimeOfDay.Evening:
+        case TimeOfDay.Night:
+            strength -= 10;
       }
         
-    case CharacterClass.mage:
-      switch()
+    case CharacterClass.Mage:
+      switch (timeOfDay) {
+        case TimeOfDay.Morning:
+        case TimeOfDay.Noon:
+        case TimeOfDay.Evening:
+        case TimeOfDay.Night:
+            strength = strength;
+      }
+      switch (weather) {
+        case Weather.Sunny:
+          strength -= 5;
+        case Weather.Rainy:
+          strength = strength;
+        case Weather.Stormy:
+          strength += 15;
+      }
       
-    case CharacterClass.archer:
-      switch()
+    case CharacterClass.Archer:
+      switch (timeOfDay) {
+        case TimeOfDay.Morning:
+        case TimeOfDay.Noon:
+        case TimeOfDay.Evening:
+        case TimeOfDay.Night:
+            strength = strength;
+      }
+      switch (weather) {
+        case Weather.Sunny:
+          strength += 10;
+        case Weather.Rainy:
+          strength -= 15;
+        case Weather.Stormy:
+          strength -= 15;
+      }
+}
+
+print ("Character: ${characterClass.name} \nWeather: ${weather.name} \nTime of Day: ${timeOfDay.name} \nStrength: ${strength}"); 
+}
